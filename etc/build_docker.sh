fced199fd412614207dcfe887670abb85fd54438
Daniel Schien <dschien@gmail.com>
2020-11-09 21:52:18 +0000

    simplication and consistency

--- etc/build_docker.sh
+++ etc/build_docker.sh
@@ -4,7 +4,7 @@ mkdir "build_tmp"
 git clone -b countries git@github.com:sust-cs-uob/eam-data-tools.git build_tmp/eam-data-tools
 git clone -b countries git@github.com:sust-cs-uob/eam-core.git build_tmp/eam-core
 
-cp build_tmp/eam-core/etc/Dockerfile build_tmp
+cp Dockerfile build_tmp
 
 cd build_tmp
-docker build --pull --rm -f "Dockerfile" -t eam-core:latest "eam-core"
+docker build --pull --rm -f "Dockerfile" -t eam-core:latest .
