a629af6b2037408cc8e5889d6d1bb800237e71aa
TBExtent <jedpreist@gmail.com>
2021-08-25 12:51:38 +0100

    added exp variable unit test

--- tests/test_models.py
+++ tests/test_models.py
@@ -150,6 +150,73 @@ class TestModels(unittest.TestCase):
             assert isclose(means['Internet Network'], 0.000000845, rel_tol=0, abs_tol=0.000000001)
             assert isclose(means['Laptop'], 0.00338, rel_tol=0, abs_tol=0.00001)
 
+    def test_ci_v2_exp(self):
+        expected_index = pd.MultiIndex.from_arrays([dates, np.zeros(13, dtype=int)], names=['time', 'samples'])
+
+        expected = pd.DataFrame(index=expected_index)
+
+        expected['CDN'] = pd.Series(index=expected_index, data=[
+            5.00000000000000E-08,
+            5.29731547179648E-08,
+            5.61231024154686E-08,
+            5.94603557501360E-08,
+            6.29960524947437E-08,
+            6.67419927085017E-08,
+            7.07106781186548E-08,
+            7.49153538438341E-08,
+            7.93700525984100E-08,
+            8.40896415253715E-08,
+            8.90898718140339E-08,
+            9.43874312681693E-08,
+            1.00000000000000E-07
+        ], dtype='pint[gigawatt_hour]')
+
+        expected['Internet Network'] = pd.Series(index=expected_index, data=[
+            5.00000000000000E-08,
+            5.29731547179648E-08,
+            5.61231024154686E-08,
+            5.94603557501360E-08,
+            6.29960524947437E-08,
+            6.67419927085017E-08,
+            7.07106781186548E-08,
+            7.49153538438341E-08,
+            7.93700525984100E-08,
+            8.40896415253715E-08,
+            8.90898718140339E-08,
+            9.43874312681693E-08,
+            1.00000000000000E-07
+        ], dtype='pint[gigawatt_hour]')
+
+        expected['Laptop'] = pd.Series(index=expected_index, data=[
+            0.000200000000000000,
+            0.000211892618871859,
+            0.000224492409661875,
+            0.000237841423000544,
+            0.000251984209978975,
+            0.000266967970834007,
+            0.000282842712474619,
+            0.000299661415375336,
+            0.000317480210393640,
+            0.000336358566101486,
+            0.000356359487256136,
+            0.000377549725072677,
+            0.000400000000000000
+        ], dtype='pint[gigawatt_hour]')
+
+        with use_test_dir():
+            runners = run(setup_parser(['-l', '-a', 'ci', '-d', '-id', get_static_path('models/ci_v2_exp.yml')]))
+            output = load_output_dataframe(runners, 'energy')
+
+            assert_pint_frame_equal(output, expected)#, rtol=1e-2)
+
+            means = get_means(output)
+
+            print(means)
+
+            assert isclose(means['CDN'], 0.000000940857687255288)#, rel_tol=0, abs_tol=0.000000001)
+            assert isclose(means['Internet Network'], 0.000000940857687255288)#, rel_tol=0, abs_tol=0.000000001)
+            assert isclose(means['Laptop'], 0.00376343074902115)#, rel_tol=0, abs_tol=0.00001)
+
     @unittest.skip("multithreading is unused, would only be for scenarios")
     def test_ci_v2_multithreaded(self):
         with use_test_dir():
