3d13446b995a7a277fbd9c4d26d64aec35ccd620
TBExtent <jedpreist@gmail.com>
2021-08-27 15:25:14 +0100

    added validation

--- src/eam_core/validate_model.py
+++ src/eam_core/validate_model.py
@@ -1,3 +1,4 @@
+import datetime
 
 statuses = ['development', 'testing', 'live', 'archived', 'test_resource']
 
@@ -33,13 +34,13 @@ def get_status(yaml_struct):
     return yaml_struct['Metadata']['status']
 
 
-def validate_model(yaml_struct, table_definitions, level='testing'):
+def validate_model(yaml_struct, level='testing'):
     validated_statuses = get_validated_statuses(level)
 
     if get_status(yaml_struct) in validated_statuses:
         assert_not_forced_invalid(yaml_struct)
 
-        validate_metadata(yaml_struct, table_definitions)
+        validate_metadata(yaml_struct['Metadata'])
 
 
 def assert_not_forced_invalid(yaml_struct):
@@ -47,41 +48,98 @@ def assert_not_forced_invalid(yaml_struct):
         raise YAMLValidationError('yaml forced to be invalid')
 
 
-def validate_metadata(yaml_struct, table_definitions):
-    assert_required_metadata_present(yaml_struct)
+def validate_metadata(metadata):
+    assert_required_metadata_present(metadata)
+    assert_metadata_types_correct(metadata)
 
-    if yaml_struct['Metadata'].get('with_group', False):
-        validate_countrified_metadata(yaml_struct, table_definitions)
+    if metadata.get('with_group', False):
+        validate_countrified_metadata(metadata)
 
 
-def validate_processes(yaml_struct, table_definitions):
+def validate_processes(yaml_struct):
     if 'Processes' not in yaml_struct:
         raise YAMLValidationError('Processes section missing from model yaml')
 
 
-def assert_required_metadata_present(yaml_struct):
-    if 'model_name' not in yaml_struct['Metadata']:
+def assert_required_metadata_present(metadata):
+    if 'model_name' not in metadata:
         raise YAMLValidationError('model_name is missing from Metadata')
 
-    if 'model_version' not in yaml_struct['Metadata']:
+    if 'model_version' not in metadata:
         raise YAMLValidationError('model_version is missing from Metadata')
 
-    if 'table_file_name' not in yaml_struct['Metadata']:
+    if 'table_file_name' not in metadata:
         raise YAMLValidationError('table_file_name is missing from Metadata')
 
+    if 'start_date' not in metadata:
+        raise YAMLValidationError('start_date is missing from Metadata')
 
-def validate_countrified_metadata(yaml_struct, table_definitions):
-    assert_required_countrified_metadata_present(yaml_struct)
-    assert_countrified_vars_are_ui(yaml_struct, table_definitions)
+    if 'end_date' not in metadata:
+        raise YAMLValidationError('end_date is missing from Metadata')
+
+    if 'sample_size' not in metadata:
+        raise YAMLValidationError('sample_size is missing from Metadata')
+
+    if 'sample_mean' not in metadata:
+        raise YAMLValidationError('sample_mean is missing from Metadata')
 
 
-def assert_required_countrified_metadata_present(yaml_struct):
-    if 'group_vars' not in yaml_struct['Metadata']:
+def assert_metadata_types_correct(metadata):
+    if not isinstance(metadata['model_name'], str):
+        raise YAMLValidationError('model_name must be a string')
+
+    if not isinstance(metadata['model_version'], str):
+        raise YAMLValidationError('model_version must be a string')
+
+    if not isinstance(metadata['table_file_name'], str):
+        raise YAMLValidationError('table_file_name must be a string')
+
+    if not isinstance(metadata['start_date'], datetime.date):
+        raise YAMLValidationError('start_date must be a date')
+
+    if not isinstance(metadata['end_date'], datetime.date):
+        raise YAMLValidationError('end_date must be a date')
+
+    if not isinstance(metadata['sample_size'], int):
+        raise YAMLValidationError('sample_size must be an integer')
+
+    if not isinstance(metadata['sample_mean'], bool):
+        raise YAMLValidationError('sample_mean must be a boolean')
+
+    if 'description' in metadata:
+        if not isinstance(metadata['description'], str):
+            raise YAMLValidationError('description must be a string')
+
+    if 'with_group' in metadata:
+        if not isinstance(metadata['with_group'], bool):
+            raise YAMLValidationError('with_group must be a boolean')
+
+
+def validate_countrified_metadata(metadata):
+    assert_required_countrified_metadata_present(metadata)
+    assert_countrified_metadata_types_correct(metadata)
+    assert_countrified_vars_are_ui(metadata)
+
+
+def assert_required_countrified_metadata_present(metadata):
+    if 'group_vars' not in metadata:
         raise YAMLValidationError('with_group is true, but group_vars is missing from Metadata')
 
-    if 'group_aggregation_vars' not in yaml_struct['Metadata']:
+    if 'group_aggregation_vars' not in metadata:
         raise YAMLValidationError('with_group is true, but group_aggregation_vars is missing from Metadata')
 
 
-def assert_countrified_vars_are_ui(yaml_struct, table_definitions):
+def assert_countrified_metadata_types_correct(metadata):
+    if not isinstance(metadata['group_vars'], list):
+        raise YAMLValidationError('group_vars must be a list')
+
+    if not isinstance(metadata['group_aggregation_vars'], list):
+        raise YAMLValidationError('group_aggregation_vars must be a list')
+
+    if 'groupings' in metadata:
+        if not isinstance(metadata['groupings'], list):
+            raise YAMLValidationError('groupings must be a list')
+
+
+def assert_countrified_vars_are_ui(metadata):
     pass
--- tests/test_validate_model.py
+++ tests/test_validate_model.py
@@ -4,6 +4,7 @@ from typing import Dict
 
 import numpy as np
 import yaml
+from ruamel import yaml
 
 from eam_core import SimulationControl, Variable
 from eam_core.YamlLoader import YamlLoader
@@ -12,12 +13,19 @@ from tests.directory_test_controller import use_test_dir
 from eam_core.validate_model import validate_model, YAMLValidationError
 
 
-class TestValidateModel(unittest.TestCase):
+def load_valid_model():
+    with use_test_dir():
+        with open('models/valid.yml') as model:
+            return yaml.load(model, Loader=yaml.RoundTripLoader)
 
-    def load_valid_model(self):
-        with use_test_dir():
-            with open('models/valid.yml') as model:
-                return yaml.load(model), None
+
+def load_valid_group_model():
+    with use_test_dir():
+        with open('models/valid_group.yml') as model:
+            return yaml.load(model, Loader=yaml.RoundTripLoader)
+
+
+class TestLevels(unittest.TestCase):
 
     def assert_invalid_throws_with_level(self, level, status):
         with self.assertRaises(Exception) as context:
@@ -74,35 +82,160 @@ class TestValidateModel(unittest.TestCase):
         self.assert_invalid_doesnt_throw_with_level('test_resource', 'archived')
         self.assert_invalid_throws_with_level('test_resource', 'test_resource')
 
+
+class ValidationTestCase(unittest.TestCase):
+
+    def assert_validate_throws_error(self, model, message):
+        with self.assertRaises(Exception) as context:
+            validate_model(model)
+
+        assert str(context.exception) == message, \
+            f'Expected exception message to be \'{message}\', but was \'{str(context.exception)}\''
+
+
+class TestValid(unittest.TestCase):
+
     def test_valid_model(self):
-        model, table_definitions = self.load_valid_model()
+        model = load_valid_model()
 
         try:
-            validate_model(model, table_definitions)
+            validate_model(model)
         except YAMLValidationError as e:
             raise AssertionError(f'Expected no errors, but \'{str(e)}\' was raised.')
 
+    def test_description_added(self):
+        model = load_valid_model()
+        model['Metadata']['Description'] = 'test'
+
+        try:
+            validate_model(model)
+        except YAMLValidationError as e:
+            raise AssertionError(f'Expected no errors, but \'{str(e)}\' was raised.')
+
+
+class TestMissing(ValidationTestCase):
+
     def test_missing_model_name(self):
-        model, table_definitions = self.load_valid_model()
+        model = load_valid_model()
         del model['Metadata']['model_name']
-
-        with self.assertRaises(Exception) as context:
-            validate_model(model, None)
-
-        assert str(context.exception) == 'model_name is missing from Metadata',\
-            f'Expected exception message to be \'model_name is missing from Metadata\', but was '\
-            f'\'{str(context.exception)}\''
+        self.assert_validate_throws_error(model, 'model_name is missing from Metadata')
 
     def test_missing_model_version(self):
-        model, table_definitions = self.load_valid_model()
+        model = load_valid_model()
         del model['Metadata']['model_version']
+        self.assert_validate_throws_error(model, 'model_version is missing from Metadata')
 
-        with self.assertRaises(Exception) as context:
-            validate_model(model, None)
+    def test_missing_table_file_name(self):
+        model = load_valid_model()
+        del model['Metadata']['table_file_name']
+        self.assert_validate_throws_error(model, 'table_file_name is missing from Metadata')
 
-        assert str(context.exception) == 'model_version is missing from Metadata',\
-            f'Expected exception message to be \'model_version is missing from Metadata\', but was '\
-            f'\'{str(context.exception)}\''
+    def test_missing_start_date(self):
+        model = load_valid_model()
+        del model['Metadata']['start_date']
+        self.assert_validate_throws_error(model, 'start_date is missing from Metadata')
+
+    def test_missing_end_date(self):
+        model = load_valid_model()
+        del model['Metadata']['end_date']
+        self.assert_validate_throws_error(model, 'end_date is missing from Metadata')
+
+    def test_missing_sample_size(self):
+        model = load_valid_model()
+        del model['Metadata']['sample_size']
+        self.assert_validate_throws_error(model, 'sample_size is missing from Metadata')
+
+    def test_missing_sample_mean(self):
+        model = load_valid_model()
+        del model['Metadata']['sample_mean']
+        self.assert_validate_throws_error(model, 'sample_mean is missing from Metadata')
+
+
+class TestMissingGroup(ValidationTestCase):
+
+    def test_missing_group_vars(self):
+        model = load_valid_group_model()
+        del model['Metadata']['group_vars']
+        self.assert_validate_throws_error(model, 'with_group is true, but group_vars is missing '
+                                                                    'from Metadata')
+
+    def test_missing_group_aggregation_vars(self):
+        model = load_valid_group_model()
+        del model['Metadata']['group_aggregation_vars']
+        self.assert_validate_throws_error(model, 'with_group is true, but group_aggregation_vars is '
+                                                                    'missing from Metadata')
+
+
+class TestTypes(ValidationTestCase):
+
+    def test_model_name_not_string(self):
+        model = load_valid_model()
+        model['Metadata']['model_name'] = 1
+        self.assert_validate_throws_error(model, 'model_name must be a string')
+
+    def test_model_version_not_string(self):
+        model = load_valid_model()
+        model['Metadata']['model_version'] = []
+        self.assert_validate_throws_error(model, 'model_version must be a string')
+
+    def test_table_file_name_not_string(self):
+        model = load_valid_model()
+        model['Metadata']['table_file_name'] = False
+        self.assert_validate_throws_error(model, 'table_file_name must be a string')
+
+    def test_description_not_string(self):
+        model = load_valid_model()
+        model['Metadata']['description'] = {}
+        self.assert_validate_throws_error(model, 'description must be a string')
+
+    def test_start_date_not_date(self):
+        model = load_valid_model()
+        model['Metadata']['start_date'] = 'aaa'
+        self.assert_validate_throws_error(model, 'start_date must be a date')
+
+    def test_end_date_not_date(self):
+        model = load_valid_model()
+        model['Metadata']['end_date'] = 65E-9
+        self.assert_validate_throws_error(model, 'end_date must be a date')
+
+    def test_sample_size_not_integer(self):
+        model = load_valid_model()
+        model['Metadata']['sample_size'] = 0.5
+        self.assert_validate_throws_error(model, 'sample_size must be an integer')
+
+    def test_sample_mean_not_bool(self):
+        model = load_valid_model()
+        model['Metadata']['sample_mean'] = 3
+        self.assert_validate_throws_error(model, 'sample_mean must be a boolean')
+
+
+class TestTypesGroup(ValidationTestCase):
+
+    def test_with_group_not_bool(self):
+        model = load_valid_group_model()
+        model['Metadata']['with_group'] = 8
+        self.assert_validate_throws_error(model, 'with_group must be a boolean')
+
+    def test_group_vars_not_list(self):
+        model = load_valid_group_model()
+        model['Metadata']['group_vars'] = 'eee'
+        self.assert_validate_throws_error(model, 'group_vars must be a list')
+
+    def test_group_aggregation_vars_not_list(self):
+        model = load_valid_group_model()
+        model['Metadata']['group_aggregation_vars'] = {'banana': 12}
+        self.assert_validate_throws_error(model, 'group_aggregation_vars must be a list')
+
+    def test_groupings_not_list(self):
+        model = load_valid_group_model()
+        model['Metadata']['groupings'] = datetime.date(2021, 1, 1)
+        self.assert_validate_throws_error(model, 'groupings must be a list')
+
+
+class TestInvalidValues(ValidationTestCase):
+
+    def test_model_version_not_semantic_versioning(self):
+        model = load_valid_model()
 
 
 if __name__ == '__main__':
